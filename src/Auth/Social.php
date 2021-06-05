9f4d02a52216eab65f94980ede4fd3ab64decfb5
void-azure <58926631+void-azure@users.noreply.github.com>
2021-06-05 19:04:58 -0400

    Create Social.php

---
+++ src/Auth/Social.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * The adless and forever free 4 player chess server.
+ *
+ * @license <https://github.com/4waychess/web-server/blob/master/license>.
+ *
+ * @link    <https://github.com/4waychess/web-server>.
+ */
+
+namespace FourWayChess\Auth\Hasher;
+
+/**
+ * Provides social login ability.
+ */
+class Social implements SocialLoginInterface
+{
+}

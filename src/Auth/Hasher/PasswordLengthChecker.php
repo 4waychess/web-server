f322e150a36be46cc8c2c1d3cfcb71fbd69b9dbd
void-azure <58926631+void-azure@users.noreply.github.com>
2021-06-05 01:55:00 -0400

    Create PasswordLengthChecker.php

---
+++ src/Auth/Hasher/PasswordLengthChecker.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+/**
+ * The adless and forever free 4 player chess server.
+ *
+ * @license <https://github.com/4waychess/web-server/blob/master/license>.
+ * @link    <https://github.com/4waychess/web-server>.
+ */
+
+namespace FourWayChess\Auth\Hasher;
+
+/**
+ * The password length checker.
+ */
+trait PasswordLengthChecker
+{
+    /**
+     * Check to see if a password is too long.
+     *
+     * @param string $password The user's password.
+     *
+     * @return bool Returns true if the password is too long
+     *              else return false.
+     */
+    public function isPasswordTooLong(string $password): bool
+    {
+        return strlen($password) > 69;
+    }
+}

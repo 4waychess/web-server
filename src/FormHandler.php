c0b50f35c4a10907d49043d82baa8467535f481b
void-azure <58926631+void-azure@users.noreply.github.com>
2021-06-05 02:38:44 -0400

    Create FormHandler.php

---
+++ src/FormHandler.php
@@ -0,0 +1,57 @@
+<?php declare(strict_types=1);
+/**
+ * The adless and forever free 4 player chess server.
+ *
+ * @license <https://github.com/4waychess/web-server/blob/master/license>.
+ * @link    <https://github.com/4waychess/web-server>.
+ */
+
+namespace FourWayChess;
+
+use Symfony\Component\Form\Extension\Csrf\CsrfExtension;
+use Symfony\Component\Form\Extension\Validator\ValidatorExtension;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\Form\Forms;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Security\Csrf\CsrfTokenManager;
+use Symfony\Component\Security\Csrf\TokenGenerator\UriSafeTokenGenerator;
+use Symfony\Component\Security\Csrf\TokenStorage\SessionTokenStorage;
+use Symfony\Component\Validator\Validation;
+
+/**
+ * Add the form handler.
+ */
+class FormHandler implements FormHandlerInterface
+{
+    /** @var \Symfony\Component\Form\FormFactoryInterface The form factory. */
+    private $formFactory;
+
+    /**
+     * Construct a new form handler.
+     *
+     * @param \Symfony\Component\HttpFoundation\Session\SessionInterface The session handler.
+     *
+     * @return void Returns nothing.
+     */
+    public function __construct(SessionInterface $session)
+    {
+        $validator = Validation::createValidator();
+        $csrfGenerator = new UriSafeTokenGenerator();
+        $csrfStorage = new SessionTokenStorage($session);
+        $csrfManager = new CsrfTokenManager($csrfGenerator, $csrfStorage);
+        $this->formFactory = Forms::createFormFactoryBuilder()
+            ->addExtension(new CsrfExtension($csrfManager))
+            ->addExtension(new ValidatorExtension($validator))
+            ->getFormFactory();
+    }
+
+    /**
+     * Get the form handler.
+     *
+     * @return \Symfony\Component\Form\FormFactoryInterface The form factory.
+     */
+    public function getFactory(): FormFactoryInterface
+    {
+        return $this->formFactory;
+    }
+}

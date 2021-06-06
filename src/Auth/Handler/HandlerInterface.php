fe313d45364816346376bb54803d47ef3e007998
void-azure <58926631+void-azure@users.noreply.github.com>
2021-06-06 18:05:16 -0400

    Add the handler interface.

---
+++ src/Auth/Handler/HandlerInterface.php
@@ -0,0 +1,17 @@
+<?php declare(strict_types=1);
+/**
+ * The adless and forever free 4 player chess server.
+ *
+ * @license <https://github.com/4waychess/web-server/blob/master/license>.
+ * @link    <https://github.com/4waychess/web-server>.
+ */
+
+namespace FourWayChess\Auth\Handler;
+
+/**
+ * The handler interface.
+ */
+interface HandlerInterface
+{
+    public function process(array $data): Response;
+}
